457512254e26cb10f4fc9ba1eb83a94af57e5b4f
Jashandeep Singh <jashandeep1659@gmail.com>
2024-04-13 18:24:30 +0530

    build basic scraper

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import { scrapper } from "./scrapper";
 const PORT = 8000;
 
+async function main() {
+  scrapper("http://fabricjs.com/docs/fabric.Rect.html");
+}
+
+main();
 const app = express();
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
